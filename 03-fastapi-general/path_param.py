a35411e76e0f94e1d82dcf49293fe9a81894de81
seongtaek <hongseongtaek@gmail.com>
2023-12-21 17:08:17 +0900

    ADD: Path param / EnumStr 실습

---
+++ 03-fastapi-general/path_param.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# Path operation 은 순서가 있다.
+# /users/me 가 호출될 경우 등록된 순서대로 호출된다.
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run("path_param:app", host="localhost", port=8000, reload=True)
---
+++ 03-fastapi-general/string_enum.py
@@ -0,0 +1,30 @@
+from enum import StrEnum
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# 리퀘스트 값을 제한하고 싶다. -> StrEnum 을 사용한다.
+# value 값이 string 인 것이 StrEnum 이다.
+class MenuDetail(StrEnum):
+    espresso = "espresso"
+    americano = "americano"
+    caffe_latte = "caffe_latte"
+
+
+@app.get("/menu/{cafe_menu}")
+async def get_cafe_menu_detail(cafe_menu: MenuDetail):
+    if cafe_menu == MenuDetail.espresso:
+        return {"cafe_menu": cafe_menu, "detail": "Espresso double shots with sugar"}
+
+    if cafe_menu.value == "caffe_latte":
+        return {"cafe_menu": cafe_menu, "detail": "Espresso + water + milk"}
+
+    if cafe_menu.value == "americano":
+        return {"cafe_menu": cafe_menu, "detail": "Espresso with 300 mL water"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("string_enum:app", host="localhost", port=8000, reload=True)
